e5a68bd3366883a0a67b1be176b6294af8fb1e09
경하 <k0nghaalee@gmail.com>
2025-10-28 09:53:46 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let even = 0;
+    let odd = 0;
+    
+    num_list.forEach(num => {
+        if(num % 2 === 0) {
+          even += 1;
+        } else {
+          odd += 1;
+        }
+    })
+    
+    return [even, odd]
+}
