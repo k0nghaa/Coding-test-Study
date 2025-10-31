74159591b72c57d2d94b22c03f5371bb4830c6c5
경하 <k0nghaalee@gmail.com>
2025-10-31 10:54:47 +0900

    [level 0] Title: 개미 군단, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,6 @@
+function solution(hp) {
+  const general = Math.floor(hp / 5); 
+  const soldier = Math.floor((hp % 5) / 3); 
+  const worker = (hp % 5) % 3;
+  return general + soldier + worker;
+}
