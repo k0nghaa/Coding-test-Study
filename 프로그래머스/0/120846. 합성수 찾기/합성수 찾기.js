d850346c73c9c17f2a0b705d3c4202202e55b59b
경하 <k0nghaalee@gmail.com>
2025-11-02 20:51:01 +0900

    [level 0] Title: 합성수 찾기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let count = 0;
+    
+    for (let i = 2; i <= n; i++) {
+        let divisor = 0;
+        
+        for (let j = 1; j <= i; j++) {
+            if(i % j === 0) divisor++
+        }
+        
+        if(divisor >= 3) count++
+    }
+    
+    return count;
+}
