eb312b3a39128114f60112d0921cf6d778e5a1d9
경하 <k0nghaalee@gmail.com>
2025-11-03 14:16:33 +0900

    [level 0] Title: 소인수분해, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let arr = [];
+    for(let i=2;i<=n;i++) {
+        while (n%i===0) {
+            arr.push(i);
+            n /= i
+        }
+    }
+    
+    return [...new Set(arr)]
+}
